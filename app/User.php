e8ecaf26b36c1786e6307542a130891457d0633f
alexcapoluongo <alexcapoluongo@icloud.com>
2022-07-08 13:54:45 +0200

    added tags in create.blade

--- app/User.php
+++ app/User.php
@@ -37,7 +37,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function userInfo() {
-        return $this->hasOne('App\UserInfo');
-    }
+    // public function userInfo() {
+    //     return $this->hasOne('App\UserInfo');
+    // }
 }
--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -14,6 +14,19 @@
           <label for="content">Content</label>
           <input type="content" class="form-control" name="content" id="content" placeholder="Write a content" value="{{ old('content')}}">
         </div>
+        
+        <h3>Tags</h3>
+        @foreach ($tags as $tag)
+            
+        <div class="form-check">
+          <input name="tags[]" class="form-check-input" type="checkbox" value="{{ $tag->id }}" id="tag-{{ $tag->id }}">
+          <label class="form-check-label" for="tag-{{ $tag->id }}">
+            {{ $tag->name }}
+          </label>
+        </div>
+
+        @endforeach
+
         <button type="submit" class="btn btn-primary mt-3">Create</button>
     </form>
 @endsection
