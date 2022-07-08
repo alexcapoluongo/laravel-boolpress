0aa369e868fc9648c3b54682f1d2f0dbb4fa5f83
alexcapoluongo <alexcapoluongo@icloud.com>
2022-07-08 14:59:52 +0200

    added delete in controller and show

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -107,7 +107,11 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::findOrFail($id);
+        $post->tags()->sync([]);
+        $post->delete();
+        
+        return redirect()->route('admin.posts.index');
     }
 
     private function getValidationRules() {
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -6,13 +6,19 @@
     <h3>Categoria {{$category ? $category->name : 'no category'}}</h3>
     <p> {{ $post->content }}</p>
     <p> {{ $post->slug }} </p>
-    
+
     @forelse ($post->tags as $tag)
-        {{ $tag->name }}{{ $loop->last ? '' : ', ' }}
+        #{{ $tag->name }}{{ $loop->last ? '' : ', ' }}
     @empty
         no one
     @endforelse
- 
-
+    <br>
     <a class="btn btn-primary" href="{{ route('admin.posts.edit', ['post'=> $post->id])}}">Edit</a>
+
+    <form action="{{ route('admin.posts.destroy', ['post' => $post->id]) }}" method="post">
+        @method('DELETE')
+        @csrf
+        
+        <button class="btn btn-danger" type="submit">Cancella</button>
+    </form>
 @endsection
