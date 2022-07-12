31d8b71b3754988b6fd027889eb5531b1fe804d7
alexcapoluongo <alexcapoluongo@icloud.com>
2022-07-12 13:56:37 +0200

    added function index in api controller

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index() {
+    $posts = Post::all();
+    return response()->json([
+        'success' => true,
+        'results' => $posts
+    ]);
+    }
 }
